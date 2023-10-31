5b4ebf95f32d6b3f719799929802a6a07d7128e8
GalG <galgabay1999@gmail.com>
2023-10-31 20:00:19 +0200

    final

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import Rating from './Rating';
 
 const Product = ({ product }) => {
+
   return (
     <Card className="my-3 p-3 rounded">
 
@@ -19,9 +19,9 @@ const Product = ({ product }) => {
                 <strong>{product.name}</strong>
             </Card.Title>
         </Link>
-        <Card.Text as="div">
+        {/*<Card.Text as="div">
             <Rating value={ product.rating } text={`${product.numReviews} reviews`  }/>
-        </Card.Text>
+        </Card.Text> */}
         <Card.Text as="h3"> {/* this is the text showing the price*/}
             ${product.price}
         </Card.Text>
